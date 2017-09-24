98d8622c2e2d781f619df4acb2b3b8d8f4043ea5
jengleberg <jeffengleberg@hotmail.com>
2017-09-23 21:13:25 -0600

    finished game

--- app.js
+++ app.js
@@ -1,130 +1,86 @@
+window.onload = start;
+var boxes = document.getElementsByTagName("td");
+var turnText = document.querySelector(".playerTurn");
+var counter = 1;
+var winCounter = 0;
+var OMoves = [];
+var XMoves = [];
 
+var winningCombinations = [[0,1,2],[3,4,5,],[6,7,8],[0,3,6],[1,4,7],
+[2,5,8],[0,4,8],[2,4,6]];
 
-
-$(document).ready(function() {
-	alert("Ready to Play");
-});
-
-window.board = {};
-
-var turnCount = 0;
-
-
-$('.cell').on('click', function() {
-	if (turnCount % 2 === 0) {
-		$(this).text('X');
-	isWinner('X');
-	}
-	else {
-		$(this).text('O');
-	isWinner('O');	
-	}
-	
-	turnCount++;
-	
-});		
-
-
-var isWinner = function() {
-
-	
-
-// Top Row winner
-
-  if ($('#1').text() === 'X' && $('#2').text() === 'X' && $('#3').text() === 'X') {
-  	isWinner = true;
-  	
-  	
-  } else if ($('#1').text() === 'O' && $('#2').text() === 'O' && $('#3').text() === 'O') {
-  	isWinner = true;
-  	
-
- // Middle Row Winner 	
-	
-  } else if ($('#4').text() === 'X' && $('#5').text() === 'X' && $('#6').text() === 'X') {
-  	isWinner = true;
-  	
-  	
-  } else if ($('#4').text() === 'O' && $('#5').text() === 'O' && $('#6').text() === 'O') {
-  	isWinner = true;
-  	
-
-// Bottom Row Winner
-
- } else if ($('#7').text() === 'X' && $('#8').text() === 'X' && $('#9').text() === 'X') {
-  	isWinner = true;
-  	
-  	
-  } else if ($('#7').text() === 'O' && $('#8').text() === 'O' && $('#9').text() === 'O') {
-  	isWinner = true;
-  	
-
- // Left Column Winner
-
-} else if ($('#1').text() === 'X' && $('#4').text() === 'X' && $('#7').text() === 'X') {
-  	isWinner = true;
-  	
-  	
-  } else if ($('#1').text() === 'O' && $('#4').text() === 'O' && $('#7').text() === 'O') {
-  	isWinner = true;
-  	
-
- //Middle Column Winner
-
-} else if ($('#2').text() === 'X' && $('#5').text() === 'X' && $('#8').text() === 'X') {
-  	isWinner = true;
-  	
-
-  } else if ($('#2').text() === 'O' && $('#5').text() === 'O' && $('#8').text() === 'O') {
-  	isWinner = true;
-  	
-
- //Right Column Winner
-
-} else if ($('#3').text() === 'X' && $('#6').text() === 'X' && $('#9').text() === 'X') {
-  	isWinner = true;
-  	
-  	
-  } else if ($('#3').text() === 'O' && $('#6').text() === 'O' && $('#9').text() === 'O') {
-  	isWinner = true;
-  	
-
- //Left Diagonal Winner
-
-} else if ($('#1').text() === 'X' && $('#5').text() === 'X' && $('#9').text() === 'X') {
-  	isWinner = true;
-  	
-  	
-  } else if ($('#1').text() === 'O' && $('#5').text() === 'O' && $('#9').text() === 'O') {
-  	isWinner = true;
-  	
-
- //Right Diagonal Winner
- 
- } else if ($('#3').text() === 'X' && $('#5').text() === 'X' && $('#7').text() === 'X') {
-  	isWinner = true;
-  	
-  	
-  } else if ($('#3').text() === 'O' && $('#5').text() === 'O' && $('#7').text() === 'O'); {
-  	isWinner = true;
-
+function start(){
+  addXandOListener();  addResetListener();
 }
 
- 
-  
-  
-  	if (isWinner === true) {
-  		if (isWinner === 'X'); 
-  		alert("Player 1 is the Winner!");
-  	} else if (isWinner === 'O') {
-  		alert("Player 2 is the Winner!");
-  	}
+function addXandOListener(){
+  for (var i = boxes.length - 1; i >= 0; i--) {
+    boxes[i].addEventListener("click", addXorO);
+  }
+}
 
-  	$('.cells').off("click");
-  
-  
+function addXorO(event) {
+  if (event.target.innerHTML.length === 0) {
+    if (counter % 2 === 0) {
+      OMoves.push(parseInt(event.target.getAttribute("data-num")));
+      event.target.innerHTML = "O";
+      event.target.setAttribute("class","O");
+      turnText.innerHTML = "X's turn";
+      counter++;
+      checkForWin(OMoves, "O");
+    }
+    else {
+      XMoves.push(parseInt(event.target.getAttribute("data-num")));
+      event.target.innerHTML = "X";
+      event.target.setAttribute("class","X");
+      turnText.innerHTML = "O's turn";
+      counter++;
+      checkForWin(XMoves, "X");
+    }
 
-};
+    if (counter>= 10) {
+      turnText.innerHTML = "GameOver!";
+      var conf = confirm("It's a draw, do you want to play again?");
+      if(conf) {
+        resetBoard();
+      }
+    }
+  }
+}
 
+function addResetListener() {
+  var resetButton = document.getElementById("newGame");
+  resetButton.addEventListener("click", resetBoard);
+}
 
+function checkForWin(movesArray, name) {
+  // loop over the first array of winning combinations
+    for (var i = 0; i < winningCombinations.length; i++) {
+      // reset the winCounter
+      winCounter = 0;
+      // loop over each individual array
+      for (var j = 0; j < winningCombinations[i].length; j++) {
+        // if the number in winning combo array is === number is moves array, add to winCounter
+        if(movesArray.indexOf(winningCombinations[i][j]) !== -1) {
+          winCounter++;
+        }
+        // if winCounter === 3 that means all 3 moves are winning combinations and the game is over
+        if(winCounter === 3){
+          alert("Game Over, " + name + " wins!");
+          resetBoard();
+        }
+      }
+    }
+}
 
+function resetBoard() {
+  for (var i = boxes.length - 1; i >=0; i--) {
+    boxes[i].innerHTML = "";
+    boxes[i].setAttribute("class","clear");
+  }
+   XMoves = [];
+   OMoves = [];
+   winCounter = 0;
+   counter = 1;
+   turnText.innerHTML="X's turn";
+}
